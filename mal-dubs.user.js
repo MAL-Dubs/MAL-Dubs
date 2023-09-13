7d7d29104c6277d917be12712fcef102b8f20a70
MAL Dubs <MAL-Dubs@users.noreply.github.com>
2023-09-13 09:53:29 -0400

    Version 0.9.43

--- mal-dubs.user.js
+++ mal-dubs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MAL (MyAnimeList) Dubs
 // @namespace    https://github.com/MAL-Dubs
-// @version      0.9.42
+// @version      0.9.43
 // @description  Labels English dubbed titles on MyAnimeList.net and adds dub only filtering
 // @author       MAL Dubs
 // @supportURL   https://github.com/MAL-Dubs/MAL-Dubs/issues
